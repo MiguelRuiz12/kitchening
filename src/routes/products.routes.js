c51ae47eca309af93d00116d9027a552361e9235
MiguelRuiz12 <ruizmiguel810@gmail.com>
2023-12-19 01:02:52 -0300

    fix: refactor productController

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { detail, add, edit } = require('../controllers/productController');
+const { detail, add, edit } = require('../controllers/productsController');
 const router = express.Router();
 
 /* GET users listing. */
 router
-    .get('/detalle/:id?', detail)
+    .get('/detalle/:id', detail)
     .get('/agregar', add)
     .get('/editar/:id?', edit)
 
